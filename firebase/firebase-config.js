4addc46ebe9d77ef38d2c6cb776c1372e26e7cbf
bryce <a0955361101@gmail.com>
2025-12-22 23:34:22 +0800

    資料庫

---
+++ firebase/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 配置資訊
+export const firebaseConfig = {
+    apiKey: "AIzaSyAGlC4eF42-wIJPUzlLTtSDDRK5C58ta9M",
+    authDomain: "stray-love-donate.firebaseapp.com",
+    projectId: "stray-love-donate",
+    storageBucket: "stray-love-donate.firebasestorage.app",
+    messagingSenderId: "1038223556368",
+    appId: "1:1038223556368:web:665cf9a2284e4018936f95"
+};
+
